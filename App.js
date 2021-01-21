eba83d0f0401fce605cf264a2a7acd8184721e8f
hiterharris <hiterharris@comcast.net>
2021-01-21 10:57:45 -0500

    render activity on button press

--- App.js
+++ App.js
@@ -23,26 +23,33 @@ const getData = () => {
 getData();
 
 const App = () => {
-  const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [newActivity, setNewActivity] = useState(false);
 
   useEffect(() => {
     fetch(endpoint)
       .then((response) => response.json())
       .then((json) => setData(json))
       .catch((error) => console.error(error));
-  }, []);
+  }, [newActivity]);
 
-  console.log('data: ', data.activity);
+  const handleClick = () => {
+    setNewActivity(data);
+  }
+
+  const renderActivity = () => {
+    if (newActivity) return <Text style={styles.activity}>{data.activity}</Text>
+  }
 
   return (
     <>
       <View style={styles.App}>
         <StatusBar barStyle="dark-content" />
-        <Text style={styles.header}>Bored?</Text>
-        <TouchableOpacity style={styles.button}>
-          <Button title='Activity Suggestion' color='white' />
-        </TouchableOpacity>
+          <Text style={styles.title}>Bored?</Text>
+          <TouchableOpacity style={styles.button} >
+            <Button title='Activity Suggestion' color='white' onPress={handleClick} />
+          </TouchableOpacity>
+          {renderActivity()}
       </View>
     </>
   );
@@ -51,17 +58,28 @@ const App = () => {
 const styles = StyleSheet.create({
   App: {
     flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
   },
-  header: {
-    marginTop: 50,
-    fontSize: 24,
+  title: {
+    fontSize: 32,
   },
   button: {
     backgroundColor: '#0059b3',
     borderRadius: 8,
     margin: 25,
+    padding: 10,
   },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '80%',
+  },
+  activity: {
+    fontSize: 24,
+    textAlign: 'center',
+  }
 });
 
 export default App;
